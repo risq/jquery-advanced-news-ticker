4d6ebd5ace1ebba2c75c6aea300b560cba14077d
risq <valentin.ledrapier@gmail.com>
2014-01-29 14:51:13 +0100

    minor modifications

--- js/newsTicker.js
+++ js/newsTicker.js
@@ -1,6 +1,3 @@
-/* global jQuery: true*/
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
 ;
 (function($, window, document, undefined) {
         'use strict';
